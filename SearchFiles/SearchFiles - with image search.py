f613db8681927f3b640867c4833650c32a6ff0ba
henkjannl <henkjanvanderpol@gmail.com>
2025-01-05 17:42:37 +0100

    Update SearchFiles - with image search.py

--- SearchFiles/SearchFiles - with image search.py
+++ SearchFiles/SearchFiles - with image search.py
@@ -1,37 +1,38 @@
 import os
 from datetime import datetime
 from PIL import Image
-import exifread
+# import exifread
 
 # STEP 1: Where to start the search
 #START_LOCATION = os.getcwd()
 #START_LOCATION = "C:\\Users\\henkj\\OneDrive\\Documents\\eclipse"
-START_LOCATION = "C:\\Users\\henkj\\OneDrive\\Fotos"
+# START_LOCATION = "C:\\Users\\henkj\\OneDrive\\Fotos"
+START_LOCATION = "C:/Users/henkj/OneDrive/01 Gezamenlijk/03 Huis/03 Zonstraat - Hengelo/2024-12-02 Schutting bokwiel/Stuklijst/STEP en PDF"
 #START_LOCATION = r"C:\Users\henkj\OneDrive\Fotos\2005"
 #START_LOCATION = r"C:\Users\henkj\OneDrive\Fotos\Amber21"
 #START_LOCATION = "C:\\"
 
 # STEP 2: How deep do we want the search to be?
-#MAX_DEPTH = 0
+MAX_DEPTH = 0
 #MAX_DEPTH = 1
-MAX_DEPTH = 999
+# MAX_DEPTH = 999
 
 # STEP 3: Filter the files that were found
 def Requirement(direntry):
-	#return True
+	return True
 	#return direntry.name.lower().endswith('cr2')
 
-	return direntry.name.lower().endswith('cr2')  or \
-           direntry.name.lower().endswith('arw')  or \
-           direntry.name.lower().endswith('jpeg') or \
-           direntry.name.lower().endswith('jpg')
+	# return direntry.name.lower().endswith('cr2')  or \
+    #        direntry.name.lower().endswith('arw')  or \
+    #        direntry.name.lower().endswith('jpeg') or \
+    #        direntry.name.lower().endswith('jpg')
 
-	return direntry.name.lower().endswith('cr2')  or \
-           direntry.name.lower().endswith('jpeg') or \
-           direntry.name.lower().endswith('jpg')  or \
-           direntry.name.lower().endswith('tif')  or \
-           direntry.name.lower().endswith('tiff') or \
-           direntry.name.lower().endswith('png') 
+	# return direntry.name.lower().endswith('cr2')  or \
+    #        direntry.name.lower().endswith('jpeg') or \
+    #        direntry.name.lower().endswith('jpg')  or \
+    #        direntry.name.lower().endswith('tif')  or \
+    #        direntry.name.lower().endswith('tiff') or \
+    #        direntry.name.lower().endswith('png')
 	#return direntry.is_dir()
 	#return ('arduino' in direntry.name.lower()) and direntry.name.lower().endswith('svg')
 	#return direntry.name.lower().endswith('h')
@@ -48,19 +49,19 @@ SORT_REVERSE = False
 
 
 # STEP 4: Define which COLUMNS in the report, in what order
-COLUMNS = ['name', 'path', 'size']
+COLUMNS = ['name']
 #COLUMNS = ['name', 'path', 'size', 'created', 'modified', 'accessed']
 #COLUMNS = ['name', 'path', 'created']
 #COLUMNS = ['name', 'exifdate', 'width', 'height']
 #COLUMNS = ['name', 'exifdate', 'size', 'width', 'height', 'path']
 
 # Fields to choose from in the COLUMNS (can be extended)
-TAGS = { 
+TAGS = {
 	'Name': 	lambda de: de.name,
 	'Path': 	lambda de: de.path,
 	'Size':		lambda de: '%d' % de.stat().st_size,
 	'Created':	lambda de: datetime.fromtimestamp(de.stat().st_ctime).strftime('%Y-%m-%d %H:%M:%S'),
-	'Modified':	lambda de: datetime.fromtimestamp(de.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S'), 
+	'Modified':	lambda de: datetime.fromtimestamp(de.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S'),
 	'Accessed':	lambda de: datetime.fromtimestamp(de.stat().st_atime).strftime('%Y-%m-%d %H:%M:%S'),
     'Exifdate':	lambda de: getExifDate(de.path),
     'Width':	lambda de: getImageWidth(de.path),
@@ -70,10 +71,10 @@ TAGS = {
 # Get date of photo from Exif
 def getExifDate(path):
 
-    # Return nothing if the path is a directory    
-    if os.path.isdir(path):  
+    # Return nothing if the path is a directory
+    if os.path.isdir(path):
         return ''
-    
+
     try:
         # First try exifread, for raw files
         with open(path, 'rb') as image_file:
@@ -90,10 +91,10 @@ def getExifDate(path):
 # Get width of photo from Exif
 def getImageWidth(path):
 
-    # Return nothing if the path is a directory    
-    if os.path.isdir(path):  
+    # Return nothing if the path is a directory
+    if os.path.isdir(path):
         return ''
-    
+
     try:
         # First try exifread, for raw files
         with open(path, 'rb') as image_file:
@@ -110,10 +111,10 @@ def getImageWidth(path):
 # Get height of photo from Exif
 def getImageHeight(path):
 
-    # Return nothing if the path is a directory    
-    if os.path.isdir(path):  
+    # Return nothing if the path is a directory
+    if os.path.isdir(path):
         return ''
-    
+
     try:
         # First try PIL
         return str(Image.open(path).size[1])
@@ -131,18 +132,18 @@ def getImageHeight(path):
 
 
 
-# Recursive function that scans the disk and add relevant files to the 
+# Recursive function that scans the disk and add relevant files to the
 # selection
 def AddToSelection(baseDir, selection, requirement, level):
 
 	try:
 		files = os.scandir(baseDir)
-		
+
 		for de in files:
-			
+
 			if requirement(de):
 				selection.append(de)
-				
+
 			if de.is_dir(follow_symlinks=False) and level<MAX_DEPTH:
 				AddToSelection(de.path, selection, requirement, level+1)
 	except:
@@ -151,12 +152,12 @@ def AddToSelection(baseDir, selection, requirement, level):
 
 # Retrieve files
 selection = []
-#AddToSelection(START_LOCATION, selection, Requirement, level=0)
+AddToSelection(START_LOCATION, selection, Requirement, level=MAX_DEPTH)
 
-for yr in range(2000, 2023):
-    loc = '{sl}\{yr}'.format(sl=START_LOCATION, yr=yr)
-    AddToSelection(loc, selection, Requirement, level=0)
-    print(loc, len(selection))
+# for yr in range(2000, 2023):
+#     loc = '{sl}\{yr}'.format(sl=START_LOCATION, yr=yr)
+#     AddToSelection(loc, selection, Requirement, level=0)
+#     print(loc, len(selection))
 
 # This way of sorting allows to sort for fields that do not end up in search results
 # However, it may require some fields to be retrieved twice, which is slower
@@ -191,24 +192,24 @@ for i,k in enumerate(COLUMNS):
 			results+=l+'\t'
 
 			# Correct spelling mistakes in COLUMNS list
-			COLUMNS[i]=l 
+			COLUMNS[i]=l
 results+='\n'
 
 # Export file results
 lines=[]
 for i,de in enumerate(selection):
-		
+
 	lines.append('\t'.join( [TAGS[k](de) for k in COLUMNS]))
-	
+
 	# Only print the first 20 lines
 	if i<20: print(de.name)
 
-	
+
 if len(selection)>=20:
 	print(':')
 
 results+='\n'.join(lines)
-	
+
 print('\nTotal of {cnt:d} files'.format(cnt=len(selection)))
 
 import pyperclip
